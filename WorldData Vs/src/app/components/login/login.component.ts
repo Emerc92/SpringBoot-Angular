159c468d24646758c67c18777406b1a35d2654ca
Emiliano Mercado <emiliano.mercado@horsa.it>
2021-06-23 12:26:29 +0200

    update angular version and structure code

--- WorldData Vs/src/app/components/login/login.component.ts
+++ WorldData Vs/src/app/components/login/login.component.ts
@@ -11,42 +11,43 @@ import { TokenService } from 'src/app/Services/token.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    default = 'World';
-    hide = true;
-    isLogged = false;
-    isLoginFail = false;
-    loginUser: Login;
-    username: string;
-    password: string;
-    roles: string[] = [];
-    errMsg: string;
+  default = 'World';
+  hide = true;
+  isLogged = false;
+  isLoginFail = false;
+  loginUser: Login;
+  username: string;
+  password: string;
+  roles: string[] = [];
+  errMsg: string;
 
   constructor(
     private tokenService: TokenService,
     private authService: AuthService,
     private router: Router,
-    ) { }
+  ) { }
 
 
   ngOnInit(): void {
-    if(this.tokenService.getToken()){
+    if (this.tokenService.getToken()) {
       this.isLogged = true;
       this.isLoginFail = false;
       this.roles = this.tokenService.getAuthorities();
     }
   }
 
-  onLogin(): void{
-    this.loginUser = new Login(this.username,this.password);
+  onLogin(): void {
+    this.loginUser = new Login(this.username, this.password);
     this.authService.login(this.loginUser).subscribe(
       data => {
         this.isLogged = true;
         this.isLoginFail = false;
-
-        this.tokenService.setToken(data.token );
+        this.tokenService.setToken(data.token);
         this.tokenService.setUsername(data.username);
         this.tokenService.setAuthorities(data.authorities.toString());
-      }, 
+        this.roles = data.authorities;
+        this.router.navigate(['/']);
+      },
       err => {
         this.isLogged = false;
         this.isLoginFail = true;
